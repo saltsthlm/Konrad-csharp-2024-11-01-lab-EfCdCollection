3035cd58c34252c1a39039421eac1a659007b900
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-09 09:56:09 +0100

    Added Models

--- CdLibrary/Models/Cd.cs
+++ CdLibrary/Models/Cd.cs
@@ -1,1 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CdLibrary.Models;
+
+public class Cd
+{
+    [Key]
+    public int Id { get; set; }
+    [StringLength(60, MinimumLength = 2)]
+    public string Name { get; set; }
+    [StringLength(60, MinimumLength = 2)]
+    public string Artist { get; set; }
+    [StringLength(200, MinimumLength = 2)]
+    public string Description { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime purchaseDate { get; set; }
+}
---
+++ CdLibrary/Models/Genre.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CdLibrary.Models;
+
+public class Genre
+{
+    [Key]
+    public int Id { get; set; }
+    [StringLength(60, MinimumLength = 2)]
+    public string Name { get; set; }
+}
