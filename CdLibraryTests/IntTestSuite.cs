3fd4d2d957a2370dc727df4475026ab63b422e5a
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-09 11:36:12 +0100

    Test configurations

--- CdLibraryTests/IntTestSuite.cs
+++ CdLibraryTests/IntTestSuite.cs
@@ -1,7 +1,27 @@
+using Testcontainers.MsSql;
+
 namespace CdLibraryTests;
 
-public class TestSuite1
+public class TestSuite1 : IAsyncLifetime
 {
+    private MsSqlContainer? _sqlServerContainer;
+
+    public async Task InitializeAsync()
+    {
+        _sqlServerContainer = new MsSqlBuilder()
+            .WithPassword("Password_2_Change_4_Real_Cases_&")
+            .Build();
+        await _sqlServerContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (_sqlServerContainer != null)
+        {
+            await _sqlServerContainer.StopAsync();
+        }
+    }
+
     [Fact]
     public void Test1()
     {
