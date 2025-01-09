42b0b496d50323bdde17479af0e68a4881633bab
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-09 10:43:12 +0100

    Removes unused usings

--- CdLibrary/Controllers/CdController.cs
+++ CdLibrary/Controllers/CdController.cs
@@ -1,10 +1,8 @@
 namespace CdLibrary.Controllers;
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CdLibrary.Models;
