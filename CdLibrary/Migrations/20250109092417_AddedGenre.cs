2e2dd8f47b5fab6f09bf49ba6a99d5b1f12574b8
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-09 10:24:55 +0100

    Updated dbcontext

---
+++ CdLibrary/Migrations/20250109092417_AddedGenre.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CdLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedGenre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GenreId",
+                table: "Cd",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cd_GenreId",
+                table: "Cd",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cd_Genre_GenreId",
+                table: "Cd",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cd_Genre_GenreId",
+                table: "Cd");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cd_GenreId",
+                table: "Cd");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Cd");
+        }
+    }
+}
