8af42446b8fae37ecfce9ad699927061cbe9f0dd
konraddrozdyk <konrad.drozdyk@gmail.com>
2025-01-09 09:30:57 +0100

    Adds entity framework into the projec

---
+++ CdLibrary/Controllers/CdController.cs
@@ -0,0 +1,21 @@
+namespace CdLibrary.Controllers;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CdLibrary.Models;
+
+public class CdController : Controller
+{
+    private readonly CdContext _context;
+
+    public CdController(CdContext context)
+    {
+        _context = context;
+    }
+
+} 
